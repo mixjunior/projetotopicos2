b59d6ce57ceb9f498b52a14bf3e17fcca7d8cece
mixjunior <zaqueu.moreira@outlook.com>
2019-11-07 15:54:07 -0300

    Add files via upload
    ct

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
-	echo("teste");
-?>
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<form action="#" method="post">
+			<p>Login:<input type="text" name="login" class="campos"></p>
+			<p>Senha:<input type="password" name="senha" class="campos"></p>
+			<input type="submit" value="Entrar" name="entrar">
+		</form>
+	</body>
+</html>
