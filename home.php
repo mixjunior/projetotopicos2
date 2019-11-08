47958f024cef756275de85bb2f47b7e3f59e2fd0
mixjunior <zaqueu.moreira@outlook.com>
2019-11-08 12:49:49 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<header>
+			<a href="index.php?sair=1">Sair</a>
+		</header>
+		<section id="main">
+			<form>
+				Nome:<input type="text" value="" name="nome" required class="campos">
+				<input type="file" name="arquivo" class="botoes">
+				<input type="submit" name="enviar" value="enviar" class="botoes">
+			</form>
+		</section>
+		<footer></footer>
+	</body>
+</html>
