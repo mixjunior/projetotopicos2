b39ecb836ee58ae05fac4bf4da43a219ca4d3945
mixjunior <zaqueu.moreira@outlook.com>
2019-11-08 13:05:03 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -1,6 +1,32 @@
 <!DOCTYPE html>
 <html>
-	<head></head>
+	<head>
+		<style>
+
+			#main{
+
+				width:900px;
+				margin:auto;
+				padding:50px;
+
+			}
+
+			#arquivos{
+
+				width:100%;
+
+
+			}
+
+			.arquivo{
+
+				width:75px;
+				margin-left:20px;
+				float:left;
+
+			}
+		</style>
+	</head>
 	<body>
 		<header>
 			<a href="index.php?sair=1">Sair</a>
@@ -11,6 +37,11 @@
 				<input type="file" name="arquivo" class="botoes">
 				<input type="submit" name="enviar" value="enviar" class="botoes">
 			</form>
+			<div id="arquivos">
+				<div class="arquivo">
+					<iframe id="teste" src='arquivos/teste.php' style='width:50px; height:50px;' frameborder='0'></iframe>
+				</div>
+			</div>
 		</section>
 		<footer></footer>
 	</body>
